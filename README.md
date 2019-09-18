a374cadef24c4487bd3e13e289afa5818edc1228
atyuwen <atyuwen@gmail.com>
2019-09-18 19:35:33 +0800

    plot the curve vis Desmo

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# aces
- Study on aces tone mapping.
+# ACES
+
+Play with ACES tone mapping in Python.
+
+Tonemapping Curve is plotted via Desmo [link](https://github.com/atyuwen/aces/raw/master/graph.png)
+![curve](https://github.com/atyuwen/aces/raw/master/curve.png)
+
+
--- aces_tonescale_1000nits.py
+++ aces_tonescale_1000nits.py
@@ -7,10 +7,55 @@ def aces_tonescale_1000nits(x):
 
 print "   mid-gray:", aces_tonescale_1000nits(0.18)
 print "paper-white:", aces_tonescale_1000nits(1.0)
-print "      curve:"
+print "    mapping:"
 for i in range(24):
     x = 0.18 * pow(2, i - 12)
     y = aces_tonescale_1000nits(x)
     print " %10.5f %10.5f" %(x, y)
 
+# Plot the mapping via desmos.
+html = '''
+<script src="https://www.desmos.com/api/v1.3/calculator.js?apiKey=dcb31709b452b1cf9dc26972add0fda6"></script>
+<div id="calculator"></div>
+<script>
+    var elt = document.getElementById('calculator');
+    var calculator = Desmos.GraphingCalculator(elt);
+    calculator.setExpression({
+        type: 'table',
+        columns: [
+            {
+                latex: 'x',
+                values: $Xs$
+            },
+            {
+                latex: 'y',
+                values: $Ys$,
+                columnMode: Desmos.ColumnModes.POINTS_AND_LINES
+            }
+        ]
+    });
+    calculator.setMathBounds({
+        left: -13,
+        right: 13,
+        bottom: -100,
+        top: 1100
+    });
+</script>
+'''
+
+Xs = [i - 12 for i in range(24)]
+Ys = [0.0] * 24
+for i in range(24):
+    x = 0.18 * pow(2, Xs[i])
+    y = aces_tonescale_1000nits(x)
+    Ys[i] = y
+
+html = html.replace("$Xs$", str(Xs))
+html = html.replace("$Ys$", str(Ys))
+f = open("graph.html", 'w')
+f.write(html)
+f.close()
+
+import os
+os.system("explorer graph.html")
 
